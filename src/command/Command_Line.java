c87a0cfed0e3626aabf468734c159bba8ab3688f
MITALI BOSE <73990307+Phoenixinfinity@users.noreply.github.com>
2020-11-23 19:20:50 +0530

    Add files via upload

---
+++ src/command/Command_Line.java
@@ -0,0 +1,13 @@
+package command;
+public class Command_Line {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a,b,sum;
+		a = Integer.parseInt(args[0]);
+		b = Integer.parseInt(args[1]);
+	             sum = a+b;
+             System.out.println("The Addition is: "+sum);
+	}
+
+}
